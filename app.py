54e8699ea4593ce89e1c763ccb637b4cc35e48ff
Daiki Matoba <telnetstat@gmail.com>
2022-09-23 14:12:40 +0900

    Add chrome option --single-process

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def build_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--single-process")
     return webdriver.Chrome(options=options)
