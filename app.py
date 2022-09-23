031664f06c2395f5f1f14f25f685662e8bfe0d6a
Daiki Matoba <telnetstat@gmail.com>
2022-09-23 13:55:38 +0900

    Add chrome option --disable-dev-shm-usage

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def build_driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(options=options)
